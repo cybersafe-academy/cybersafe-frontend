816c4c569aa04b4a7ba6d7ea52711fc4230a1ddd
Jovi <joao.marquesvale@gmail.com>
2023-03-28 22:40:24 -0300

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,119 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="body">
+    <div class="section left-section d-flex flex-column">Vazia</div>
 
-    <div class="wrapper">
-      <HelloWorld msg="Cybersafe Academy" />
+    <div class="section right-section d-flex flex-column">
+      <span class="text-h3"> Welcome to Cybersafe Academy! </span>
+
+      <span class="text-h4"> Slogan here! </span>
+
+      <v-text-field
+        clearable
+        label="Login"
+        prepend-icon="mdi-account"
+        variant="outlined"
+      ></v-text-field>
+
+      <v-text-field
+        clearable
+        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="show1 ? 'text' : 'password'"
+        label="Password"
+        prepend-icon="mdi-lock"
+        @click:append="show1 = !show1"
+        variant="outlined"
+      ></v-text-field>
+
+      <span @click="switchForgotPassword" class="link text-right"> Forgot password? </span>
+
+      <v-btn @click="login" variant="tonal"> Login </v-btn>
+
+      <div>
+        <span class="signup-text"> Don't have an account? </span>
+
+        <span @click="switchSignup" class="link"> Sign up </span>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+import type { AxiosInstance } from 'axios'
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance
+    catTags: string[]
+  }
+}
+
+export default defineComponent({
+  data() {
+    return {
+      show1: false,
+      password: '',
+      rules: {
+        required: (v: any) => !!v || 'Required.',
+        min: (v: any) => v.length >= 8 || 'Min 8 characters'
+      }
+    }
+  },
+
+  computed: {},
+
+  methods: {
+    switchSignup(): void {
+      console.log('Switching to signup')
+    },
+
+    switchForgotPassword(): void {
+      console.log('Switching to forgot password')
+    },
+
+    async login() {
+      console.log('Logging in')
+      console.log(await this.$axios.get('https://www.google.com.br'))
+    }
+  }
+})
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.body {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  padding: 0 10%;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.section {
+  width: 50%;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.signup-text {
+  font-size: 14px;
+  color: #000000;
+  font-weight: 400;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.link {
+  font-size: 14px;
+  color: #2600ff;
+  font-weight: 700;
+}
+
+.link:hover {
+  cursor: pointer;
+  text-decoration: underline;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,32 @@ import App from './App.vue'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+// Axios
+import axios from './globals/plugins/axios'
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
 })
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(vuetify)
+app.use(axios, {
+  baseUrl: 'https://jsonplaceholder.typicode.com'
+})
 
 app.mount('#app')
