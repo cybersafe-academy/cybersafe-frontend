85d8ff0d62cd5d8771a07b6d18a556f34f75b013
Bruno Bergamini <bruno.bergamini@outlook.com>
2023-11-12 20:39:27 -0300

    WIP

--- src/pages/Analytics.vue
+++ src/pages/Analytics.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="tableContent">
     <v-row>
+      <p class="text-h2">Exati Tecnologia</p>
+    </v-row>
+    <v-row>
+      <v-col class="pa-12 d-flex justify-center">
+        <div class="d-flex flex-column justify-center align-center">
+          <p class="text-h6">Número de Usuários:</p>
+          <p clas="text-center">{{ analyticsData.numberOfUsers }}</p>
+        </div>
+        <v-progress-circular
+          class="mr-12"
+          v-model="analyticsData.accuracyInQuizzes"
+          :size="150"
+          :width="10"
+          bg-color="grey"
+          :color="getForeColor(analyticsData.accuracyInQuizzes)"
+        >
+          <p class="text-h6">{{ analyticsData.accuracyInQuizzes }}%</p>
+        </v-progress-circular>
+        <v-progress-circular
+          v-model="analyticsData.courseCompletion"
+          class="mr-12"
+          :size="150"
+          :width="10"
+          bg-color="grey"
+          :color="getForeColor(analyticsData.courseCompletion)"
+          >{{ analyticsData.courseCompletion }}%</v-progress-circular
+        >
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col>
-        <v-row>
-          <v-col>
-            <v-progress-circular
-              v-model="testsAccuracy"
-              :size="150"
-              :width="10"
-              bg-color="amber"
-              color="yellow"
-            >
-              <p class="text-h6">{{ testsAccuracy }}%</p>
-            </v-progress-circular>
-          </v-col>
-          <v-col><v-progress-circular :size="150"></v-progress-circular></v-col>
-          <v-col><v-progress-circular :size="150"></v-progress-circular></v-col>
-        </v-row>
-        <v-row>
-          <canvas id="my-chart" width="500" height="300"></canvas>
-        </v-row>
+        <canvas id="my-chart" width="500" height="300"></canvas>
       </v-col>
     </v-row>
   </div>
@@ -31,21 +44,73 @@ export default {
   name: 'AnalyticsComponent',
   data() {
     return {
-      testsAccuracy: 100
+      analyticsData: {} as any
     }
   },
-  mounted() {
+  methods: {
+    getForeColor(progress: number) {
+      if (progress < 50) {
+        return 'red'
+      }
+      if (progress < 70) {
+        return 'yellow'
+      }
+      if (progress <= 100) {
+        return 'green'
+      }
+      return 'grey'
+    },
+    getBackColor(progress) {
+      switch (progress) {
+        case progress < 50:
+          return 'red'
+        case progress < 70:
+          return 'yellow'
+        case progress <= 100:
+          return 'green'
+        default:
+          return 'grey'
+      }
+    }
+  },
+  async mounted() {
+    this.analyticsData = (await this.$axios.get('/analytics/data')).data
+
     new Chart(document.getElementById('my-chart'), {
-      type: 'line',
+      type: 'bar',
       data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        labels: this.analyticsData.mbtiCount.map(
+          (mbtiCount: any) => mbtiCount.mbtiType
+        ),
         datasets: [
           {
-            fill: false,
-            label: '2018 Sales',
-            data: [300, 700, 450, 750, 450]
+            label: 'MBTI Types',
+            data: this.analyticsData.mbtiCount.map(
+              (mbtiCount: any) => mbtiCount.count
+            ),
+            backgroundColor: [
+              'red',
+              'blue',
+              'green',
+              'yellow',
+              'purple',
+              'orange',
+              'pink',
+              'brown',
+              'gray',
+              'cyan'
+              // Add more colors as needed for each bar
+            ],
+            borderWidth: 1 // Optional: You can customize the border width
           }
         ]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
       }
     })
   }
